ca74e6631cabf8b28a7a1b24701bf7ff3e885113
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-03-31 21:11:38 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,17 +1,47 @@
 import torch
 import torch.nn as nn
-class EmotionModel(nn.Module):
-    def __init__(self , num_classes):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3 , 16 , 3 , padding=1)
-        self.conv2 = nn.Conv2d(16 , 32 , 3 , padding=1)
-        self.conv3 = nn.Conv2d(32 , 64 , 3 , padding=1)
-        self.pool = nn.MaxPool2d(2)
-        self.fc = nn.Linear(64 * 12 * 12, num_classes)
 
-    def forward(self , x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = self.pool(torch.relu(self.conv3(x)))
-        x = torch.flatten(x , 1)
-        return self.fc(x)
+class EmotionModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        # Block 1
+        self.conv1_1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1_1 = nn.BatchNorm2d(16)
+        self.conv1_2 = nn.Conv2d(16, 16, 3, padding=1)
+        self.bn1_2 = nn.BatchNorm2d(16)
+        # Block 2
+        self.conv2_1 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2_1 = nn.BatchNorm2d(32)
+        self.conv2_2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.bn2_2 = nn.BatchNorm2d(32)
+        # Block 3
+        self.conv3_1 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3_1 = nn.BatchNorm2d(64)
+        self.conv3_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.bn3_2 = nn.BatchNorm2d(64)
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.fc1 = nn.Linear(64 * 12 * 12, 96)
+        self.dropout = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(96, num_classes)
+    def forward(self, x):
+        # Block 1
+        x = torch.relu(self.bn1_1(self.conv1_1(x)))
+        x = torch.relu(self.bn1_2(self.conv1_2(x)))
+        x = self.pool(x)
+        # Block 2
+        x = torch.relu(self.bn2_1(self.conv2_1(x)))
+        x = torch.relu(self.bn2_2(self.conv2_2(x)))
+        x = self.pool(x)
+
+        # Block 3
+        x = torch.relu(self.bn3_1(self.conv3_1(x)))
+        x = torch.relu(self.bn3_2(self.conv3_2(x)))
+        x = self.pool(x)
+
+        x = torch.flatten(x, 1)
+
+        x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
+        return self.fc2(x)
