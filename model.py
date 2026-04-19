dcecb9dc08d6bfb04efcd26a4524d901726a42d8
Siddhaarth <siddhaarthsharma360@gmail.com>
2026-04-19 23:17:31 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -6,28 +6,28 @@ class EmotionModel(nn.Module):
         super().__init__()
 
         # Block 1
-        self.conv1_1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.bn1_1 = nn.BatchNorm2d(16)
-        self.conv1_2 = nn.Conv2d(16, 16, 3, padding=1)
-        self.bn1_2 = nn.BatchNorm2d(16)
+        self.conv1_1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.bn1_1 = nn.BatchNorm2d(32)
+        self.conv1_2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.bn1_2 = nn.BatchNorm2d(32)
 
         # Block 2
-        self.conv2_1 = nn.Conv2d(16, 32, 3, padding=1)
-        self.bn2_1 = nn.BatchNorm2d(32)
-        self.conv2_2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.bn2_2 = nn.BatchNorm2d(32)
+        self.conv2_1 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2_1 = nn.BatchNorm2d(64)
+        self.conv2_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.bn2_2 = nn.BatchNorm2d(64)
 
         # Block 3
-        self.conv3_1 = nn.Conv2d(32, 64, 3, padding=1)
-        self.bn3_1 = nn.BatchNorm2d(64)
-        self.conv3_2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn3_2 = nn.BatchNorm2d(64)
+        self.conv3_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn3_1 = nn.BatchNorm2d(128)
+        self.conv3_2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn3_2 = nn.BatchNorm2d(128)
 
         self.pool = nn.MaxPool2d(2)
 
-        self.fc1 = nn.Linear(64 * 12 * 12, 96)
-        self.dropout = nn.Dropout(0.3)
-        self.fc2 = nn.Linear(96, num_classes)
+        self.fc1 = nn.Linear(128 * 6 * 6, 256)
+        self.dropout = nn.Dropout(0.4)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = self.conv1_1(x)
